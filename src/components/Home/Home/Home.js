d6ab5e6fe02c831c9961781eb4a88e9474e75854
umar <unzippinglinux@gmail.com>
2021-05-11 00:34:30 +0600

    purchase card fixed

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Header/Header";
 import LuxuryDivan from "../LuxuryDivan/LuxuryDivan";
+import PurchaseCard from "../PurchaseCard/PurchaseCard";
 import PurchaseShow from "../PurchaseShow/PurchaseShow";
 import SofaClassic from "../SofaCalssic/SofaClassic";
 import TrendyChair from "../TrendyChair/TrendyChair";
--- src/components/Home/PurchaseCard/PurchaseCard.js
+++ src/components/Home/PurchaseCard/PurchaseCard.js
@@ -4,25 +4,48 @@ import trendyChairImg from "./../../../Images/furnitureForShow/go-with-the-trend
 const PurchaseCard = () => {
   return (
     <div
-      style={{ border: "0.1rem solid ", height: "30vh", margin: "1rem" }}
-      className="col-md-4 col-sm-12  d-flex align-items-center p-1 mx-1"
+      style={{
+        color: "white",
+        background: "rgba(0, 0, 0, 0.9)",
+        border: "1px solid rgba(255, 255, 255, .5)",
+        width: "24rem",
+        height: "15rem",
+      }}
+      className="row"
     >
-      <div className="row">
-        <div className="col-md-6 d-flex justify-content-center align-items-center">
-          <img
-            width="80rem"
-            src={trendyChairImg}
-            alt=""
-            className="img-fluid rounded m-auto d-block"
-          />
-        </div>
-        <div className="col-md-6 text-white">
-          <h6>Lorem ipsum dolor sit.</h6>
+      {/* image container div */}
+      <div
+        style={{ borderRight: "1px solid rgba(255, 255, 255, .5)" }}
+        className="col-sm-6 col-md-6 d-flex justify-content-center align-items-center"
+      >
+        <img src={trendyChairImg} alt="" className="img-fluid" />
+      </div>
+
+      {/* information container div */}
+      <div className="col-sm-6 col-md-6 d-flex flex-column justify-content-between align-items-center">
+        <div
+          style={{
+            height: "10rem",
+            marginTop: ".5rem",
+            textAlign: "justify",
+          }}
+        >
+          <h6 className="text-center">Lorem ipsum dolor sit.</h6>
           <small>Lorem ipsum dolor sit amet consectetur.</small>
-          <div className="row d-flex  justify-content-center align-items-center">
-            <h6>$500</h6>
-            <button className="btn btn-success-alt">Purchase Now</button>
-          </div>
+        </div>
+        <div
+          style={{ width: "12rem" }}
+          className="d-flex align-items-center justify-content-between my-2"
+        >
+          <h5 className="m-auto">$500</h5>
+          <button
+            style={{
+              borderRadius: "0",
+            }}
+            className="btn btn-info mx-auto"
+          >
+            Purchase
+          </button>
         </div>
       </div>
     </div>
