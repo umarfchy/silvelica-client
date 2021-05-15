b18be0c09f9f1ea4d91ce159553e5fedd30191a3
umarfchy <umarfchy@gmail.com>
2021-05-16 01:27:10 +0600

    redux introduced

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Home from "./components/Home/Home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./actions/index";
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="appDisplay">
-      <Home></Home>
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch(increment())}>Increase value</button>
+      <button onClick={() => dispatch(decrement())}>Decrease value</button>
+      {/* <Home></Home> */}
     </div>
   );
 }
